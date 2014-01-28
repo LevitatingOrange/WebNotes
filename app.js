b2f03283245acff0580fc176ea217211164eac40
levo55 <lennart@vogelsangs.net>
2014-01-28 15:17:42 +0100

    first tests

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
-    res.send("Hello World");
-}
+    res.render("index");
+});
+
+app.listen(16000);
+console.log('Listening on port 16000');
