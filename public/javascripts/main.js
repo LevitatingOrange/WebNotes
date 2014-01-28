9904405b56ba876d2dd3f49bbb123e019663d8e2
levo55 <lennart@vogelsangs.net>
2014-01-28 18:02:48 +0100

    added lots of functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,58 @@
-$(".note").draggable();
+
+//changes the size of a note
+var changeSize = function() {
+    var thisEl = $(this);
+    var parent = thisEl.parent();
+    if (parent.hasClass("small-note")) {
+        thisEl.removeClass("fa-compress");
+        thisEl.removeClass("fa-expand");
+        thisEl.addClass("fa-expand");
+        parent.removeClass("small-note");
+        parent.addClass("medium-note");
+    } else if (parent.hasClass("medium-note")) {
+        thisEl.removeClass("fa-expand");
+        thisEl.removeClass("fa-compress");
+        thisEl.addClass("fa-compress");
+        parent.removeClass("medium-note");
+        parent.addClass("large-note");
+    } else {
+        thisEl.removeClass("fa-compress");
+        thisEl.removeClass("fa-expand");
+        thisEl.addClass("fa-expand");
+        parent.removeClass("large-note");
+        parent.addClass("small-note");     
+    }
+};
+
+var revealNoteUI = function() {
+    $(this).children(".i-size").fadeIn();
+};
+
+var hideNoteUI = function() {
+    $(this).children(".i-size").fadeOut();
+};
+
+
+//performs a deep copy of a node already existing as an orignial
+var addNote = function(type, id) {
+    var newNote = $("#original").children("."+type+"-note").clone(true, true);
+    newNote.attr("id", id);
+    newNote.draggable({ containment: "parent" });
+    $("#main").append(newNote);
+};
+
+var addTextToNote = function(id, text) {
+    $("#"+id).children(".note-text").append("<p>"+text+"</p>",id);
+};
+
+var removeNote = function(id) {
+    $("#"+id).remove();
+};
+
+
+//all event handlers
+$(".i-size").on("click", changeSize);
+$(".note").on("mouseenter", revealNoteUI);
+$(".note").on("mouseleave", hideNoteUI);
+
+
